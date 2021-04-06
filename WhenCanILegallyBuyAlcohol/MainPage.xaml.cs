52bcf57a9c4b0894c9efb78b4378a55f2f895142
davidmichaelm <davemarquardt@gmail.com>
2021-04-05 20:22:35 -0500

    * .gitignore: Asks for age and tells you how many years until you   are 21
    * WhenCanILegallyBuyAlcohol.sln:
    * App.xaml:
    * App.xaml.cs:
    * MainPage.xaml:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * MainActivity.cs:
    * AboutAssets.txt:
    * WhenCanILegallyBuyAlcohol.csproj:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * WhenCanILegallyBuyAlcohol.Android.csproj:
    * icon_round.xml:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ WhenCanILegallyBuyAlcohol/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WhenCanILegallyBuyAlcohol
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void SubmitButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                int age = Int32.Parse(AgeInput.Text);
+                int yearsUntilLegal = 21 - age;
+
+                string labelText;
+                if (yearsUntilLegal > 0) {
+                    labelText = $"You can buy alcohol in {yearsUntilLegal} year{(yearsUntilLegal > 1 ? "s" : "")}.";
+                }
+                else
+                {
+                    labelText = "You can legally buy alcohol!";
+                }
+                YearsLabel.Text = labelText;
+            }
+            catch
+            {
+                YearsLabel.Text = "Input error";
+            }
+        }
+    }
+}
